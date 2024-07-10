469c0b6cffaa9493da8effd1ea167e65c3865c55
caioneves05 <ncaio037@gmail.com>
2024-07-09 22:57:39 -0300

    feat: adding live provider

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -6,7 +6,7 @@ type Error = {
 }
 
 const customFetch = async (url: string, options: RequestInit) => {
-    const accessToken = localStorage.getItem('accesToken');
+    const accessToken = localStorage.getItem('access-token');
 
     const headers = options.headers as Record<string, string>;
 
--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,7 +1,9 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, { GraphQLClient, liveProvider as graphqlLiveProvider } from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
+import { createClient } from "graphql-ws";
 
 export const baseUrl = process.env.BASE_URL_API
+export const wsUrl = process.env.WS_URL
 
 export const client = new GraphQLClient(baseUrl!, {
     fetch: (url: string, options: RequestInit) => {
@@ -12,3 +14,19 @@ export const client = new GraphQLClient(baseUrl!, {
         }
     }
 })
+
+export const wsClient = typeof window !== "undefined" ? createClient({
+    url: wsUrl as string,
+    connectionParams: () => {
+        const accessToken = localStorage.getItem("access-token")
+
+        return {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }
+    }
+}) : undefined
+
+export const dataProvider = graphqlDataProvider(client)
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined
